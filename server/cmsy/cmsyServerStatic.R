440b71266a009c31dc9529f33cf2b3a701d3af04
Enrico Anello <enrico.anello@gmail.com>
2020-04-14 18:00:49 +0200

    unused files removed

--- server/cmsy/cmsyServerStatic.R
+++
@@ -1,9 +0,0 @@
-output$selectStockInFile <- renderUI({
-  inFileCmsy <- input$fileCmsy
-  if (is.null(inFileCmsy)) {
-    return(NULL)
-  }
-  a <- read.csv(inFileCmsy$datapath)
-  
-  selectInput("stock", "Select a stock", sort(unique(a$Stock)))
-})
