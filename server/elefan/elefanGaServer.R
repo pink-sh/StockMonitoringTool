39cf650942c6a35e25ea4b84157f01d5bae3878d
tokami <tobm@aqua.dtu.dk>
2021-03-14 15:21:45 +0100

    issue #54

--- server/elefan/elefanGaServer.R
+++ server/elefan/elefanGaServer.R
@@ -615,7 +615,40 @@ footer = NULL
     output$plot_elefanFit <- renderPlot({
         req(elefan_ga$results)
         par(mar=c(5,5,2,1))
-        GA::plot(elefan_ga$results$resGA$gafit)
+        ## GA::plot.ga()
+        cex.points = 0.7
+        col = c("green3", "dodgerblue3",
+                adjustcolor("green3", alpha.f = 0.1))
+        pch = c(16, 1)
+        lty = c(1,2)
+        legend = TRUE
+        grid = graphics:::grid
+        object <- elefan_ga$results$resGA$gafit
+        is.final <- !(any(is.na(object@summary[, 1])))
+        iters <- if (is.final) 1:object@iter else 1:object@maxiter
+        summary <- object@summary
+        ylim <- c(max(apply(summary[, c(2, 4)], 2,
+                            function(x) min(range(x, na.rm = TRUE, finite = TRUE)))),
+                  max(range(summary[,1], na.rm = TRUE, finite = TRUE)))
+        plot(iters, summary[, 1], type = "n", ylim = ylim, xlab = "Generation",
+             ylab = "Fitness value (Rn)")
+        if (is.final & is.function(grid)) grid(equilogs = FALSE)
+        points(iters, summary[, 1], type = ifelse(is.final, "o","p"),
+               pch = pch[1], lty = lty[1], col = col[1], cex = cex.points)
+        points(iters, summary[, 2], type = ifelse(is.final, "o","p"),
+               pch = pch[2], lty = lty[2], col = col[2], cex = cex.points)
+        if(is.final){
+            polygon(c(iters, rev(iters)), c(summary[, 4], rev(summary[,1])), border = FALSE, col = col[3])
+        }else{
+            title(paste("Iteration", object@iter), font.main = 1)
+        }
+        if(is.final & legend){
+            inc <- !is.na(col)
+            legend("bottomright", legend = c("Best", "Mean", "Median")[inc],
+                   col = col[inc], pch = c(pch, NA)[inc],
+                   lty = c(lty, 1)[inc], lwd = c(1, 1, 10)[inc],
+                   pt.cex = c(rep(cex.points, 2), 2)[inc], inset = 0.02)
+        }
     })
     output$title_elefanFit <- renderText({
         req(elefan_ga$results)
